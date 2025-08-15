00fa9d34b5d3ed1cf6d8ef11dba75b375f21da6d
fayze3 <noturfayze@gmail.com>
2025-08-16 02:48:49 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~JrthCSpK#e84SYOqms6na6Y0zuaougFmVsG2yFJHbbuLR6vrAg9I",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "923364627909", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: " " //put your country timeZone....leave blank if u don't know.
 };
